d1cb3e1bf6acfe9e1a73887fac91fa3dd7a38dab
VietAnhNguyen <vietanhit95@gmail.com>
2016-08-20 21:10:39 +0700

    First Verssion

---
+++ Demo_B2/Controllers/BaiVietController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo_B2.Models;
+
+namespace Demo_B2.Controllers
+{
+    public class BaiVietController : Controller
+    {
+        //
+        // GET: /BaiViet/
+        public ActionResult Index()
+        {
+            using (Entities eT = new Entities())
+            {
+                List<BaiViet> lstBaiViet = eT.BaiViets.ToList();
+                return View(lstBaiViet);
+            }
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(FormCollection f)
+        {
+            string tenBaiviet = f["ten"].ToString();
+            string loaiBaiviet = f["loai"].ToString();
+            string tieuDe = f["tieude"].ToString();
+            string noiDung = f["noidung"].ToString();
+            using (Entities eT = new Entities())
+            {
+                BaiViet bv = new BaiViet();
+                bv.TenBaiViet = tenBaiviet;
+                bv.LoaiBaiViet = loaiBaiviet;
+                bv.TieuDe = tieuDe;
+                bv.NoiDungChiTiet = noiDung;
+
+                eT.BaiViets.Add(bv);
+                eT.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using (Entities eT = new Entities())
+            {
+                BaiViet bV = eT.BaiViets.FirstOrDefault(k => k.ID == id);
+                return View(bV);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(FormCollection f, int id)
+        {
+            using (Entities eT = new Entities())
+            {
+                string tenBaiviet = f["ten"].ToString();
+                string loaiBaiviet = f["loai"].ToString();
+                string tieuDe = f["tieude"].ToString();
+                string noiDung = f["noidung"].ToString();
+                BaiViet bv = eT.BaiViets.FirstOrDefault(k => k.ID == id);
+                bv.TenBaiViet = tenBaiviet.Trim();
+                bv.LoaiBaiViet = loaiBaiviet.Trim();
+                bv.TieuDe = tieuDe.Trim();
+                bv.NoiDungChiTiet = noiDung.Trim(); ;
+                eT.SaveChanges(); ;
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (Entities eT = new Entities())
+            {
+                BaiViet bV = eT.BaiViets.FirstOrDefault(k => k.ID == id);
+                eT.BaiViets.Remove(bV);
+                eT.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Demo_B2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_B2.Startup))]
+namespace Demo_B2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
